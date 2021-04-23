24bda13f8e522645fcaf8a553c9504e677f259d4
M_abedalnabi <abedalnabi96@gmail.com>
2021-04-23 13:08:40 +0300

    Firstly Disian

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 console.log("*********Project-1**********")
+
+
+
+  const hello = () => {
+      let a = document.querySelector("p")
+      a.style.color = "red"
+      console.log("Hello");
+      console.log(a);
+    
+
+  };
+
+  const hello1 = () => {
+    console.log("Hello");
+  };
+
+  
